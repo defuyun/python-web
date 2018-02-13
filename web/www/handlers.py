f788cf608df2b969b83dfbc186bf86e7844cbd06
defuyun <carlliu2012@gmail.com>
2018-02-13 15:42:55 -0800

    add nodejs

--- web/www/handlers.py
+++ web/www/handlers.py
@@ -1,1 +1,12 @@
 #! /usr/bin/python3
+
+import asyncio
+
+from aiohttp import web
+from coroweb import get, post
+
+@get('/')
+async def index(request):
+    return {
+        '__template__' : 'index.html'
+    }
